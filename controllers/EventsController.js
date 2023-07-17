02739ed46b4998ca34e5fff38eb2e02bdb318f57
shawn-goh24 <ahshawngoh@gmail.com>
2023-07-17 20:46:11 +0800

    New route to edit rsvp

--- controllers/EventsController.js
+++ controllers/EventsController.js
@@ -1,6 +1,99 @@
 const db = require("../db/models/index");
 
-const { Event, UserEvent, Calendar } = db;
+const { Event, UserEvent, Calendar, User } = db;
+
+async function getEventWithUser(req, res) {
+  const { eventId, userId } = req.params;
+  try {
+    const event = await UserEvent.findOne({
+      where: {
+        eventId: eventId,
+        userId: userId,
+      },
+    });
+
+    return res.json(event);
+  } catch (err) {
+    return res.status(400).json({ error: true, msg: err });
+  }
+}
+
+async function getUsersInEvent(req, res) {
+  const { eventId } = req.params;
+  try {
+    const usersInEvent = await Event.findByPk(eventId, {
+      include: User,
+    });
+    return res.json(usersInEvent);
+  } catch (err) {
+    return res.status(400).json({ error: true, msg: err });
+  }
+}
+
+async function getRsvpCount(req, res) {
+  const { eventId } = req.params;
+  try {
+    const yes = await UserEvent.count({
+      where: {
+        eventId: eventId,
+        rsvpId: 1,
+      },
+    });
+    const no = await UserEvent.count({
+      where: {
+        eventId: eventId,
+        rsvpId: 2,
+      },
+    });
+    const maybe = await UserEvent.count({
+      where: {
+        eventId: eventId,
+        rsvpId: 3,
+      },
+    });
+
+    const rsvpCount = {
+      yes: yes,
+      no: no,
+      maybe: maybe,
+    };
+
+    return res.json(rsvpCount);
+  } catch (err) {
+    return res.status(400).json({ error: true, msg: err });
+  }
+}
+
+async function editEventWithUser(req, res) {
+  const { eventId, userId } = req.params;
+  try {
+    const rsvpId = req.body;
+    console.log(rsvpId);
+    let userEventToEdit = await UserEvent.findOne({
+      where: {
+        eventId: eventId,
+        userId: userId,
+      },
+    });
+
+    console.log(userEventToEdit);
+
+    if (userEventToEdit === null) {
+      const newUserEvent = await UserEvent.create({
+        userId: userId,
+        eventId: eventId,
+        roleId: 2,
+        rsvpId: rsvpId.rsvpId,
+      });
+      return res.json(newUserEvent);
+    } else {
+      await userEventToEdit.update(rsvpId);
+      return res.json(userEventToEdit);
+    }
+  } catch (err) {
+    return res.status(400).json({ error: true, msg: err });
+  }
+}
 
 // Get add event - 1. add to Event table, 2. add to UserEvent table
 async function addEvent(req, res) {
@@ -65,7 +158,11 @@ async function deleteEvent(req, res) {
 }
 
 module.exports = {
+  getEventWithUser,
+  getUsersInEvent,
   addEvent,
   editEvent,
   deleteEvent,
+  editEventWithUser,
+  getRsvpCount,
 };
--- routers/EventsRouter.js
+++ routers/EventsRouter.js
@@ -3,6 +3,10 @@ const eventsController = require("../controllers/EventsController");
 
 const router = express.Router();
 
+router.get("/userevent/:eventId/:userId", eventsController.getEventWithUser);
+router.get("/userevent/:eventId/", eventsController.getUsersInEvent);
+router.get("/rsvpcount/:eventId", eventsController.getRsvpCount);
+router.put("/userevent/:eventId/:userId", eventsController.editEventWithUser);
 router.post("/add", eventsController.addEvent); // Add event
 router.put("/edit/:eventId", eventsController.editEvent); // Edit a single event
 router.delete("/delete/:eventId/:calendarId", eventsController.deleteEvent); // Delete a single event
